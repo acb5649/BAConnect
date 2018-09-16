5e5d78bd2b91e7f2167834295ceb66e4879cbc09
estrayer <estrayer@users.noreply.github.com>
2018-09-16 17:04:12 -0400

    starting registration page
    I've started from my Lab1 assignment, but gutted most of the php stuff. I figured it would be easier to just write the stuff later than go through and trying to adapt the code to our new needs.

---
+++ form.php
@@ -0,0 +1,99 @@
+<?php
+	require_once "functions.php";
+
+	$msg = "";
+	$term = "You must agree to the terms and conditions";
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!-- Name: Eric Strayer -->
+<!-- Date: 9/16/2018 -->
+<!-- File Name: form.php -->
+
+<html lang="EN" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<title>Functions Demo</title>
+	<style type = "text/css">
+  		h1, h2 {
+    		text-align: center;
+  		}
+	</style>
+
+	</head>
+
+	<body>
+
+		<?php
+			if (isset($_POST['enter']))
+			{
+				//just naming the variables now
+
+				$firstName = "";
+				$lastName = "";
+				$middleName = "";
+				$address = "";
+				$gender = "";
+				$phone = "";	//user may have multiple phone numbers
+
+				$password = "";
+				$username = "";
+
+				$email = "";
+				$student = false;	//user can be a student, a working professional, or both
+				$working = false;
+				$field = "";	//field of profession, if working
+				$employer = "";	//name of employer, if working
+				$numDegrees = 0;
+				$Degrees[] = null; //array containing $Degrees
+												//degrees may need to be objects
+			}
+		?>
+
+		<form action="testPage.php" method="post">
+			<?php
+				print $msg;
+				$msg = "";
+			?>
+			<br />
+
+			First Name: <input type="text" maxlength = "50" value="" name="firstName" id="firstName"   /> <br />
+			Last Name: <input type="text" maxlength = "50" value="" name="lastName" id="lastName"   />  <br />
+
+			Email: <input type="text" maxlength = "50" value="" name="email" id="email"   /> <br />
+		<!--	Confirm Email: <input type="text" maxlength = "50" value="" name="cemail" id="email"   /> <br /> -->
+
+			Password: <input type="text" maxlength = "50" value="" name="pwd" id="pwd"   />(Must be longer than 12 characters and contains at least 1 digit) <br />
+
+		<!--	Confirm Password: <input type="text" maxlength = "50" value="" name="confirmPwd" id="confirmPwd"   /> <br /> -->
+
+			Gender:
+				<input type = "radio" name = "gender" value = "Male" checked = "checked" />Male
+				<input type = "radio" name = "gender" value = "Female" />Female <br />
+			Country:
+			<select  name = "country">
+				<?php print countryList(); ?>
+			</select>
+
+			<br />
+
+			Status:
+			<br />
+
+			<input type="checkbox" name = "student" value=1 />
+			student
+			<br />
+			<input type="checkbox" name = "Working Professional" value=1 />
+			Working Professional
+			<br />
+
+			Education:
+			<br />
+			 Degree:
+
+
+
+			<input name="enter" class="btn" type="submit" value="Submit" />
+		</form>
+	</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Name: Eric Strayer
+Date: 9/16/2018
+File Name: functions.php
+*/
+
+
+/* This function will generate the list of countries for a drop down list
+		I don't know if we'll add more countries or not. Depending on how many we add,
+		we'll need a more efficient way of making the list.
+*/
+function CountryList() {
+	$list = '<option value = "USA">United States of America</option>
+  		    <option value = "MYA">Myanmar</option>';
+
+	return $list;
+}
+
+function DegreeTypeList() {
+//	$list = '<option value = ""'
+}
+?>
