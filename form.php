f92e8b49499d6aee219787c815b8f00eb132db5b
estrayer <estrayer@users.noreply.github.com>
2018-09-24 16:06:03 -0400

    started powerPoint, added username box

--- form.php
+++ form.php
@@ -35,15 +35,15 @@
                 if (number != 0) {
                     container.appendChild(document.createElement("br"));
                 }
-                
+
                 var parent = document.createElement("div")
                 parent.className = "educationMember"
                 parent.id = "member_" + number
-                
+
                 var select = document.createElement("select")
                 select.innerHTML = '<?php print DegreeTypeList(); ?>'
                 parent.appendChild(select)
-                
+
                 var schoolNameInput = document.createElement("input");
                 schoolNameInput.type = "text"
                 schoolNameInput.maxlength = 50
@@ -52,7 +52,7 @@
                 schoolNameInput.name = "schoolName_" + number
                 schoolNameInput.id = "schoolName_" + number
                 parent.appendChild(schoolNameInput)
-                
+
                 var majorInput = document.createElement("input");
                 majorInput.type = "text"
                 majorInput.maxlength = 50
@@ -61,9 +61,9 @@
                 majorInput.name = "major_" + number
                 majorInput.id = "major_" + number
                 parent.appendChild(majorInput)
-                
+
                 parent.appendChild(document.createTextNode("Year Graduated:"))
-                
+
                 var graduationYearInput = document.createElement("input")
                 graduationYearInput.type = "number"
                 graduationYearInput.maxlength = 4
@@ -71,16 +71,16 @@
                 graduationYearInput.name = "gradYear_" + number
                 graduationYearInput.id = "gradYear_" + number
                 parent.appendChild(graduationYearInput)
-                
+
                 //<input name = "addEntry" class = "btn" type = "button" value = "Add degree" onclick = "addField()" />
-                
+
                 var deleteInputFieldButton = document.createElement("input")
                 deleteInputFieldButton.className = "btn"
                 deleteInputFieldButton.type = "button"
                 deleteInputFieldButton.value = "Remove Degree"
                 deleteInputFieldButton.onclick = function(){removeField(number);}
                 parent.appendChild(deleteInputFieldButton)
-                
+
                 container.appendChild(parent);
         }
     </script>
@@ -122,12 +122,13 @@
 			<br />
 
 			First Name: <input type="text" maxlength = "50" value="" name="firstName" id="firstName"   /> <br />
+      Middle Name: <input type = "text" maxlength = "50" value = "" name = "middleName" id = "middleName" /> <br />
 			Last Name: <input type="text" maxlength = "50" value="" name="lastName" id="lastName"   />  <br />
 
 			Email: <input type="text" maxlength = "50" value="" name="email" id="email"   /> <br />
 		<!--	Confirm Email: <input type="text" maxlength = "50" value="" name="cemail" id="email"   /> <br /> -->
-
-			Password: <input type="text" maxlength = "50" value="" name="pwd" id="pwd"   />(Must be longer than 12 characters and contains at least 1 digit) <br />
+      Username: <input type = "text" maxlength ="50" value = "" name = "username" id = "username" /> <br />
+			Password: <input type="text" maxlength = "50" value="" name="pwd" id="pwd"   /> <br />
 		<!--	Confirm Password: <input type="text" maxlength = "50" value="" name="confirmPwd" id="confirmPwd"   /> <br /> -->
 			Gender:
 				<input type = "radio" name = "gender" value = "Male" checked = "checked" />Male
