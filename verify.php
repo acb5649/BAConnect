7a74326baa7c8e474145b52b0973f3dca55082e9
Alex Bridges <alebridg@iu.edu>
2018-10-16 16:53:45 -0400

    add verification files

--- verify.php
+++ verify.php
@@ -1,16 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <meta content="utf-8" http-equiv="encoding">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BAConnect</title>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  </head>
-  <body>
-    <div class="w3-content" style="max-width:2000px;margin-top:46px">
+<?php
+require_once "functions.php";
 
-    </div>
-  </body>
-</html>
+$code = filter_input(INPUT_GET, "code");
+$email = filter_input(INPUT_GET, "email", FILTER_VALIDATE_EMAIL);
+
+if ($code && $email) {
+  if valididateCode($code, $email) {
+    
+    header("Location: success.php");
+  } else {
+    header("Location: failure.php");
+  }
+}
+
+?>
