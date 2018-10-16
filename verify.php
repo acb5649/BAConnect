3cf505b18950237e87b473d0e51f5598a3eb7950
Alex Bridges <alebridg@iu.edu>
2018-10-16 15:14:20 -0400

    add code generator function

---
+++ verify.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="utf-8" http-equiv="encoding">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BAConnect</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <div class="w3-content" style="max-width:2000px;margin-top:46px">
+
+    </div>
+  </body>
+</html>
