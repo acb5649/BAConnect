859fd257cfc2a31a6a3b573ac14f73b0b66ed903
alebridg <alebridg@iupui.edu>
2018-10-17 20:34:46 -0400

    Fix account validation

--- verify.php
+++ verify.php
@@ -5,7 +5,7 @@ $code = filter_input(INPUT_GET, "code");
 $email = filter_input(INPUT_GET, "email", FILTER_VALIDATE_EMAIL);
 
 if ($code && $email) {
-  if (valididateCode($code, $email)) {
+  if (verifyCode($code, $email)) {
     header("Location: success.php");
   } else {
     header("Location: failure.php");
