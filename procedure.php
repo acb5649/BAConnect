2ab9bf12cc7b2803601cd5a9b259d1aa9451d0c3
youngjon <youngjon@iupui.edu>
2018-10-17 22:54:19 -0400

    code

--- procedure.php
+++ procedure.php
@@ -5,28 +5,37 @@ class procedure{
 	public $accountNumber;
 	public $sessionID;
 	public $accountType;
-	private $connectDB;
 	
-	function __construct($username=NULL, $password=NULL, $accountNumber=NULL,$sessionID=NULL, $accountType = -1, $connectDB=NULL){
+	function __construct($username=NULL, $password=NULL, $accountNumber=NULL,$sessionID=NULL, $accountType = 0){
 		$this->username=$username;
 		$this->password=$password;
 		$this->accountNumber=$accountNumber;
 		$this->sessionID=$sessionID;
 		$this->accountType =$accountType;
-		$this->connectDB=$connectDB;
 	}
 	public function Connect(){
 		try {
-			$this->connectDB = new PDO("mysql:host=localhost;dbname=youngjon_db", "youngjon", "youngjon");
+			$connectDB = new PDO("mysql:host=localhost;dbname=youngjon_db", "youngjon", "young");
 			$isConnected = true;
 		} catch (PDOException $e) {
 			echo $e->getMessage();
 		}
 	}
-	public function disDB(){
-		$this->connectDB=null;
-	}
 	public function verify($username,$password){
+		$connectDB = mysql_connect("localhost", "youngjon", "young","youngjon_db");
+		mysql_select_db("youngjon_db");
+		$query = mysql_query("SELECT * FROM Account WHERE username='$username'");
+		$numrows = mysql_num_rows($query);
+		if ($numrows!=0){
+			//while loop
+			while ($row = mysql_fetch_assoc($query)){
+				$this->username = $row['username'];
+				$this->password = $row['password'];
+				$this->accountType = $row['type'];
+			}
+		}
+		$connectDB=null;
+		
 		
 	}
 	public function GetPrivelages(){
@@ -35,14 +44,27 @@ class procedure{
 			
 			return $this->accountType;
 		}else{
-			$this->accountType=null;
+			$this->accountType=0;
 			return $this->accountType;
 		}
 	}
 	public function GetSESSIONID(){
 		if(isset($_GET['sd'])){
 			$this->sessionID= $_GET['sd'];
-		}
+			$connectDB = mysql_connect("localhost", "youngjon", "Jcyoung1","youngjon_db");
+			mysql_select_db("youngjon_db");
+			$query = mysql_query("SELECT * FROM Account WHERE sessionid='$this->sessionID'");
+			$nRows = mysql_num_rows($query);
+			if ($nRows!=0){
+				//while loop
+				while ($row = mysql_fetch_assoc($query)){
+					$this->accountNumber = $row['account_ID'];
+				}
+			}
+			$connectDB=null;
+		}else{
+			print "<script>document.getElementById('loginModal').style.display='block';</script>";
+		}	
 	}
 	public function Share($loc){
 		Header ("Location:".$loc.".php?sd=".$this->sessionID);
