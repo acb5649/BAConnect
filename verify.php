fd14b3ab3b8a2a5fde4b0ea190a2eab0d332cff8
alebridg <alebridg@iupui.edu>
2018-11-20 11:03:21 -0500

    Add PDO protection

--- verify.php
+++ verify.php
@@ -11,12 +11,14 @@ if ($code && $email) {
         if (verifyCode($code, $email)) {
             echo "<script> console.log(" . $email . ") </script>";
             $con = Connection::connect();
-            $stmt = $con->prepare("select account_ID from Information where email_address = '" . $email . "'");
+            $stmt = $con->prepare("select account_ID from Information where email_address = ?");
+            $stmt->bindValue(1, $email, PDO::PARAM_STR);
             $stmt->execute();
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
             $account_id = $row['account_ID'];
 
-            $stmt = $con->prepare("UPDATE Account SET active = '1' WHERE account_ID = '" . $account_id . "'");
+            $stmt = $con->prepare("UPDATE Account SET active = '1' WHERE account_ID = ?");
+            $stmt->bindValue(1, $account_id, PDO::PARAM_INT);
             $stmt->execute();
 
             header("Location: success.php");
