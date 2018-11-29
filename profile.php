bbd934152a47394e402857c65c68e6e46df16765
Alex Bridges <alebridg@iu.edu>
2018-11-29 11:18:33 -0500

    minor visual tweaks

--- profile.php
+++ profile.php
@@ -846,7 +846,7 @@ function formatPendingMentorships($profile_account_id) {
 
                     <?php if ($allowEdit) {
                         // show resume
-                        echo '<hr><p class="w3-display-container" id="profile_resume"><button class="w3-button w3-half w3-lime w3-cell w3-margin-top" type="button" name="upload" onclick="document.getElementById(\'uploadResumeModal\').style.display=\'block\'">Upload Resume</button><button class="w3-button w3-half w3-lime w3-cell w3-margin-top" type="button" name="download" onclick="downloadResume();">Download Resume</button><br><br>';
+                        echo '<hr><p class="w3-display-container" id="profile_resume"><button class="w3-button w3-half w3-lime w3-cell" type="button" name="upload" onclick="document.getElementById(\'uploadResumeModal\').style.display=\'block\'">Upload Resume</button><button class="w3-button w3-half w3-lime w3-cell" type="button" name="download" onclick="downloadResume();">Download Resume</button><br>';
                     } ?>
 
                     <br>
