7c231f5bdc4782462cd33716696ef17b2ebfdd37
alebridg <alebridg@iupui.edu>
2018-12-03 12:09:47 -0500

    Revert "undo"
    This reverts commit bcfe5dab1ce6deef21fed6505a42c88745c907f3.

--- profile.php
+++ profile.php
@@ -573,7 +573,6 @@ function formatPendingMentorships($profile_account_id) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BAConnect Profile</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <script src="js/closeModals.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
